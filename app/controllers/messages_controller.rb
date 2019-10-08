1cdbd1abf81ce081a38e0115f4f77012c984cdec
chanoku0201 <okudakazunori@okudakazunorinoMacBook-Air.local>
2019-10-08 15:50:13 +0900

    chatsidemainreview

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,29 @@
+class MessagesController < ApplicationController
+
+  def index
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def edit
+
+  end
+end
