5678ff3816f813c653b03134282d520afd2a494d
chanoku0201 <okudakazunori@okudakazunorinoMacBook-Air.local>
2019-10-08 17:29:07 +0900

    変更完了しました

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,27 +3,4 @@ class MessagesController < ApplicationController
   def index
   end
 
-  def new
-
-  end
-
-  def create
-
-  end
-
-  def show
-
-  end
-
-  def update
-
-  end
-
-  def destroy
-
-  end
-
-  def edit
-
-  end
 end
