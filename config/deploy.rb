eab83a46f1a6d63a1d39e8c23f8a35bda78acd78
chanoku0201 <okudakazunori@okudakazunorinoMacBook-Air.local>
2019-10-21 20:01:16 +0900

    デぷろい

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,13 +39,13 @@
 # set :ssh_options, verify_host_key: :secure
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.11.0'
+lock '3.11.2'
 
 # Capistranoのログの表示に利用する
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:chanoku0201/ChatSpaceChanoku.git'
+set :repo_url,  'git@github.com:chanoku0201/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
